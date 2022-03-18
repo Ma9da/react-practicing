e594a08154e0c80be29bbedf7b0ad27c98f5fced
ma9da <dodymasry25@gmail.com>
2022-03-18 16:38:29 +0200

    create and display home component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
-
+import Home from './home/Home';
 function App() {
-  const title = 'welcome to the new blog'
-  const likes = 50;
-  const person = {
-    name: "magda",
-    age: 24
-  };
-  const link = "https://www.google.com/";
   return (
     <div className="App">
       <div className="content">
-        <h1> { title } </h1>
-        <p>Liked { likes } times</p>
-        <p>
-          name: {person.name}
-          age: {person.age}
-        </p>
-        <a href={link}>Google</a>
+        <Home/>
       </div>
     </div>
   );
---
+++ src/home/Home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+    return (
+        <div className="home">
+            homepage
+        </div>
+    )
+}
+
+export default Home
