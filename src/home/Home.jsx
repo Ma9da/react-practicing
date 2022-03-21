17fd350ee03cdd60797a866c5f152cc3dd8bc651
ma9da <dodymasry25@gmail.com>
2022-03-21 15:28:33 +0200

    added: function with  arguments

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,12 +2,16 @@ function Home() {
     const handelClick = () =>{
         console.log("hello there");
     }
+    const printName = (name) =>{
+        console.log("your name is " + name)
+    }
     return (
         <>
             <div className="home">
                 homepage
             </div>
             <button onClick={handelClick}>click me</button>
+            <button onClick={()=>{printName("yoshi")}}>print</button>
         </>
     )
 }
