c4820b65c5f8ad7e5f2a61a4050c8fb8fc317b2a
ma9da <dodymasry25@gmail.com>
2022-03-21 16:27:57 +0200

    added: outputing a list

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -3,38 +3,47 @@ import { useState } from "react";
 function Home() {
     const [name, setName] = useState("anonymous");
     const [age, setAge] = useState(0);
-    const [blogs, setBlogs] = useState({
+    const [blogs, setBlogs] = useState([
         { title: "my new website", body: "lorem ipsum...", author: "mario", id: 1 },
         { title: "welcome party", body: "lorem ipsum...", author: "yoshi", id: 2 },
         { title: "web dev top tip", body: "lorem ipsum...", author: "mario", id: 3 },
-    })
-const handelClick = () => {
-    console.log("hello there");
-}
-const printName = (name) => {
-    console.log("your name is " + name)
-}
-const handelEvent = (e) => {
-    console.log(e.target);
-}
-return (
-    <>
-        <div className="home">
-            homepage
-            <h2>
-                User name: {name}
-            </h2>
-            <h2>
-                age: {age}
-            </h2>
-            <button onClick={() => { setName("Arther") }}>change user name</button>
-            <button onClick={() => { setAge(17) }}>change user age</button>
-            <button onClick={handelClick}>click me</button>
-            <button onClick={() => { printName("yoshi") }}>print</button>
-            <button onClick={(e) => { handelEvent(e) }}>trigger</button>
-        </div>
-    </>
-)
+    ])
+    const handelClick = () => {
+        console.log("hello there");
+    }
+    const printName = (name) => {
+        console.log("your name is " + name)
+    }
+    const handelEvent = (e) => {
+        console.log(e.target);
+    }
+    return (
+        <>
+            <div className="home">
+                homepage
+                <h2>
+                    User name: {name}
+                </h2>
+                <h2>
+                    age: {age}
+                </h2>
+                <button onClick={() => { setName("Arther") }}>change user name</button>
+                <button onClick={() => { setAge(17) }}>change user age</button>
+                <button onClick={handelClick}>click me</button>
+                <button onClick={() => { printName("yoshi") }}>print</button>
+                <button onClick={(e) => { handelEvent(e) }}>trigger</button>
+                <div className="blogs">
+                    {blogs.map((blog) => (
+                        <div className="blog" key={blog.id}>
+                            <h2>{blog.title}</h2>
+                            <p>{blog.body}</p>
+                            <blockquote> {blog.author} </blockquote>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Home
