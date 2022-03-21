20ee82280f95e9fb4f72ec9fc0567d11cdd94875
ma9da <dodymasry25@gmail.com>
2022-03-21 15:57:44 +0200

    added: useState

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 function Home() {
     const [name, setName] = useState("anonymous");
+    const [age, setAge] = useState(0);
     const handelClick = () => {
         console.log("hello there");
     }
@@ -16,8 +17,13 @@ function Home() {
             <div className="home">
                 homepage
             </div>
-            <h2>User name: {name}</h2>
-            <button onClick={()=>{setName("Arther")}}>change user name</button>
+            <h2>User name: {name}
+            </h2>
+            <h2>
+                age: {age}
+            </h2>
+            <button onClick={() => { setName("Arther") }}>change user name</button>
+            <button onClick={()=>{setAge(17)}}>change user age</button>
             <button onClick={handelClick}>click me</button>
             <button onClick={() => { printName("yoshi") }}>print</button>
             <button onClick={(e) => { handelEvent(e) }}>trigger</button>
