e3711571359034654bf2246526e8fd104d6bf38c
ma9da <dodymasry25@gmail.com>
2022-03-22 15:01:53 +0200

    added: post request

--- src/create/Create.jsx
+++ src/create/Create.jsx
@@ -4,10 +4,22 @@ const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
+    const handelSubmit = (e) => {
+        e.preventDefault()
+        const blog = { title, body, author }
+        fetch('http://localhost:8000/blogs', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(blog)
+        })
+            .then(() => {
+                console.log("new blog added");
+            })
+    }
     return (
         <>
             <h2>add a new blog</h2>
-            <form>
+            <form onSubmit={handelSubmit}>
                 <label htmlFor="title">blog title:</label>
                 <input type="text"
                     name="title"
@@ -23,18 +35,19 @@ const Create = () => {
                     rows="10"
                     required
                     value={body}
-                    onChange={(e)=>{setBody(e.target.value)}}
-                    >
+                    onChange={(e) => { setBody(e.target.value) }}
+                >
                 </textarea>
                 <label htmlFor="author">blog author:</label>
-                <select name="authors" id="author" value={author} onChange={(e)=>{setAuthor(e.target.value)}}>
+                <select name="authors" id="author" value={author} onChange={(e) => { setAuthor(e.target.value) }}>
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
+                <button type="submit">submit</button>
             </form>
             <article className="blog">
                 <h2>{title}</h2>
-                <p>{body}</p> 
+                <p>{body}</p>
                 <span>{author}</span>
             </article>
         </>
