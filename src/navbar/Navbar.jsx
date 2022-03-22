2f9d9cab7b42f976c97d05f9a470ed4718d1272e
ma9da <dodymasry25@gmail.com>
2022-03-22 13:13:57 +0200

    added: router links

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom"
 function Navbar() {
     return (
         <nav className="navbar">
             <h1>Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/">New Blog</a>
+                <link to="/">Home</link>
+                <link to="/create">New Blog</link>
             </div>
         </nav>
     )
