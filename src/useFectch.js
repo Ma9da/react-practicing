54dad94240f46f7517a5bf11e12f24b70f8cf874
ma9da <dodymasry25@gmail.com>
2022-03-22 13:39:07 +0200

    added: cleanup and abort

--- src/useFectch.js
+++ src/useFectch.js
@@ -5,7 +5,8 @@ function useFectch(url) {
     const [isPending, setIsPending] = useState(true)
     const [error, setError] = useState(null);
     useEffect(() => {
-        fetch(url)
+        const abortCont = new AbortController()
+        fetch(url, { signal: abortCont.signal })
             .then(res => {
                 if (!res.ok) {
                     throw Error("couldn't fetch data for that resource")
@@ -18,9 +19,14 @@ function useFectch(url) {
                 setError(null)
             })
             .catch(err => {
-                setIsPending(false)
-                setError(err.message)
+                if (err.name === "AbourtError") {
+                    console.log("fetch abortef")
+                } else {
+                    setIsPending(false)
+                    setError(err.message)
+                }
             })
+        return () => abortCont.abort()
     }, [url]);
     return {
         data, isPending, error
